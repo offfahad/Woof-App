871f55dd86e2e19f61c926f7b3d29751c8dc2d88
offfahad <mughalfahad544@gmail.com>
2023-07-04 17:25:04 +0500

    animation-added

--- app/src/main/java/com/example/woofapp/MainActivity.kt
+++ app/src/main/java/com/example/woofapp/MainActivity.kt
@@ -7,6 +7,9 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -140,7 +143,15 @@ fun DogItem(
     Card(
         modifier = modifier
     ) {
-        Column() {
+        Column(
+            modifier = Modifier
+                .animateContentSize(
+                    animationSpec = spring(
+                        dampingRatio = Spring.DampingRatioNoBouncy,
+                        stiffness = Spring.StiffnessMedium
+                    )
+                )
+        ) {
             Row(
                 modifier = modifier
                     .fillMaxWidth()
